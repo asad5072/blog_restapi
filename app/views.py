8e1e05d07439617028a0492d07637195a3d7e904
Asad <mmonir5072@gmail.com>
2024-06-05 07:05:20 +0600

    asdfsf

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return JsonResponse({'post': serializer.data})
--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', views.post_list, name='post-list'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
