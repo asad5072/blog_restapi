63f6f5abdcb5d8cb2ee0eaa6e2b0d643294f9ea8
asad5072 <mmonir5072@gmail.com>
2024-06-11 19:26:17 +0600

    asdfasd

---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <!-- <NuxtWelcome /> -->
+       <p class="text-2xl font-bold text-red-400">Home Page</p>
+       <div v-for="post in posts.post" :key="post.id">
+        <p>Product</p>   
+        {{ post.title }}
+       
+        </div>
+{{ posts }}
+    </div>
+  </template>
+
+<script setup>
+    const {data: posts} =await useFetch('http://127.0.0.1:8000/api/')
+</script>
